d036665ab43272d059b25d10d2ad72edaa310fcd
Eloy Gomez <eloy@indeos.es>
2010-12-04 12:44:10 +0000

    Añadimos bloques try catch para evitar que se cuelgue al arrancar si el servidor omvc no esta disponible.

--- src/org/opensixen/dev/omvc/StartupModelValidator.java
+++ src/org/opensixen/dev/omvc/StartupModelValidator.java
@@ -3,11 +3,14 @@
  */
 package org.opensixen.dev.omvc;
 
+import java.net.ConnectException;
+
 import org.compiere.model.MClient;
 import org.compiere.model.MSysConfig;
 import org.compiere.model.ModelValidationEngine;
 import org.compiere.model.ModelValidator;
 import org.compiere.model.PO;
+import org.compiere.util.CLogger;
 import org.compiere.util.Env;
 import org.opensixen.omvc.client.proxy.OMVCAuthServiceProxy;
 import org.opensixen.omvc.client.proxy.RemoteConsoleProxy;
@@ -20,21 +23,27 @@ import org.opensixen.osgi.interfaces.IModelValidator;
  * - Register Auth server on login
  * 
  * 
- * @author Eloy Gomez
- * Indeos Consultoria http://www.indeos.es
- *
+ * @author Eloy Gomez Indeos Consultoria http://www.indeos.es
+ * 
  */
 public class StartupModelValidator implements IModelValidator {
 
-	/* (non-Javadoc)
-	 * @see org.compiere.model.ModelValidator#initialize(org.compiere.model.ModelValidationEngine, org.compiere.model.MClient)
+	private CLogger log = CLogger.getCLogger(getClass());
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.compiere.model.ModelValidator#initialize(org.compiere.model.
+	 * ModelValidationEngine, org.compiere.model.MClient)
 	 */
 	@Override
 	public void initialize(ModelValidationEngine engine, MClient client) {
-		engine.addModelChange(MSysConfig.Table_Name, this);		
+		engine.addModelChange(MSysConfig.Table_Name, this);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.compiere.model.ModelValidator#getAD_Client_ID()
 	 */
 	@Override
@@ -44,34 +53,46 @@ public class StartupModelValidator implements IModelValidator {
 
 	/**
 	 * Register Auth Proxy
+	 * 
 	 * @see org.compiere.model.ModelValidator#login(int, int, int)
 	 */
 	@Override
 	public String login(int AD_Org_ID, int AD_Role_ID, int AD_User_ID) {
-		// Register Auth proxy
-		OSXServiceConnectionHandler handler = OSXServiceConnectionHandler.getInstance();
+		try {
+			// Register Auth proxy
+			OSXServiceConnectionHandler handler = OSXServiceConnectionHandler.getInstance();
 
-		// Set connection Handlers
-		RemoteConsoleProxy.getInstance().setServiceConnectionHandler(handler);
-		RevisionDownloaderProxy.getInstance().setServiceConnectionHandler(handler);
-		OMVCAuthServiceProxy.getInstance().setServiceConnectionHandler(handler);
-		OMVCAuthServiceProxy.getInstance().register();
+			// Set connection Handlers
+			RemoteConsoleProxy.getInstance().setServiceConnectionHandler(handler);
+			RevisionDownloaderProxy.getInstance().setServiceConnectionHandler(handler);
+			OMVCAuthServiceProxy.getInstance().setServiceConnectionHandler(	handler);
+			OMVCAuthServiceProxy.getInstance().register();
+		} catch (Exception e) {
+			log.warning("No se puede contectar con el servidor OMVC");
+		}
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.compiere.model.ModelValidator#modelChange(org.compiere.model.PO, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.compiere.model.ModelValidator#modelChange(org.compiere.model.PO,
+	 * int)
 	 */
 	@Override
 	public String modelChange(PO po, int type) throws Exception {
-		if (type == ModelValidator.CHANGETYPE_NEW || type == ModelValidator.CHANGETYPE_CHANGE)	{
+		if (type == ModelValidator.CHANGETYPE_NEW
+				|| type == ModelValidator.CHANGETYPE_CHANGE) {
 			OSXServiceConnectionHandler.getInstance().fireConnectionChange();
 		}
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.compiere.model.ModelValidator#docValidate(org.compiere.model.PO, int)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.compiere.model.ModelValidator#docValidate(org.compiere.model.PO,
+	 * int)
 	 */
 	@Override
 	public String docValidate(PO po, int timing) {
